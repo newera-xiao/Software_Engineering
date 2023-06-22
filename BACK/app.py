1d31414603574ae8c47697e5023f86b7afd17f31
HaomingYu <1813442316@qq.com>
2023-06-22 11:13:58 +0800

    reformat code

--- BACK/app.py
+++ BACK/app.py
@@ -2,27 +2,30 @@ import os
 import json
 import db
 from flask import Flask, Response, request, session
+
 # from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'database.sqlite'),
-    )
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'database.sqlite'),
+)
 db.init_app(app)
+# print(app.config['DATABASE'])
 
-#--------------------------------------------------------------
+# --------------------------------------------------------------
 #                          1.登录模块                          |
-#--------------------------------------------------------------
+# --------------------------------------------------------------
 @app.route('/api/auth/login', methods=["POST"])
 def login() -> Response | str:
-    '''
+    """
     用户登录接口
-    '''
+    """
+    res = {}
     error = None
-    status = 'fali'
+    status = 'fail'
     username = request.get_json()['username']
-    password = request.get_json()['password']
+    in_password = request.get_json()['password']
     database = db.get_db()
     user = database.execute(
         'SELECT * FROM user WHERE username = ?', (username,)
@@ -32,14 +35,14 @@ def login() -> Response | str:
         error = '用户' + str(username) + '未注册'
     # elif not check_password_hash(user['password'], password):
     #     error = '密码错误'
-    elif user['password'] != password:
+    elif user['password'] != in_password:
         error = '密码错误'
 
     if error is None:
         session.clear()
         session['userId'] = user['id']
         status = 'success'
-    
+
     if status == 'success':
         res = {
             'status': status,
@@ -49,7 +52,7 @@ def login() -> Response | str:
                 'accessToken': ''
             }
         }
-    elif status == 'fali':
+    elif status == 'fail':
         res = {
             'status': status,
             'data': {
@@ -61,21 +64,23 @@ def login() -> Response | str:
 
     return json.dumps(res)
 
+
 @app.route('/api/auth/register', methods=["POST"])
 def register() -> Response | str:
-    '''
+    """
     用户注册接口
-    '''
+    """
+    res = {}
     error = None
-    status = 'fali'
+    status = 'fail'
     # print(request.get_json())
     username = request.get_json()['username']
-    password = request.get_json()['password']
+    in_password = request.get_json()['password']
     database = db.get_db()
 
     if not username:
         error = '用户名不能为空'
-    elif not password:
+    elif not in_password:
         error = '密码不能为空'
 
     if error is None:
@@ -86,13 +91,13 @@ def register() -> Response | str:
             # )
             database.execute(
                 "INSERT INTO user (username, password) VALUES (?, ?)",
-                (username, password)
+                (username, in_password)
             )
             database.commit()
             status = 'success'
         except database.IntegrityError:
             error = '用户名已被注册'
-    
+
     if status == 'success':
         res = {
             'status': status,
@@ -100,7 +105,7 @@ def register() -> Response | str:
                 "message": 'Registration successful'
             }
         }
-    elif status == 'fali':
+    elif status == 'fail':
         res = {
             'status': status,
             'data': {
@@ -109,11 +114,12 @@ def register() -> Response | str:
         }
     return json.dumps(res)
 
+
 @app.route('/api/auth/logout', methods=["POST"])
 def logout() -> Response | str:
-    '''
+    """
     用户注销接口
-    '''
+    """
     res = {
         'status': 'success',
         'data': {
@@ -122,11 +128,13 @@ def logout() -> Response | str:
     }
     return json.dumps(res)
 
+
 @app.route('/api/auth/user/password', methods=["PUT"])
 def password() -> Response | str:
-    '''
+    """
     修改用户密码接口
-    '''
+    """
+    res = {}
     error = None
     status = 'error'
     old_password = request.get_json()['old_password']
@@ -182,18 +190,19 @@ def password() -> Response | str:
 
     return json.dumps(res)
 
-#--------------------------------------------------------------
+
+# --------------------------------------------------------------
 #                 2.1 康复计划管理模块                          |
-#--------------------------------------------------------------
+# --------------------------------------------------------------
 @app.route('/api/rehabilitation/plans', methods=["POST"])
 def create_plans() -> Response | str:
-    '''
+    """
     手动创建康复计划接口
-    '''
+    """
     user_info = request.get_json()
     print(user_info)
     res = {
-        "status": "success", # success or error
+        "status": "success",  # success or error
         "data": {
             "planId": "plan123",
             "message": "Rehabilitation plan created successfully"
@@ -201,24 +210,26 @@ def create_plans() -> Response | str:
     }
     return json.dumps(res)
 
+
 @app.route('/api/rehabilitation/plans', methods=["PUT"])
 def update_plans() -> Response | str:
-    '''
+    """
     更新康复计划接口
-    '''
+    """
     user_info = request.get_json()
     print(user_info)
     res = {
-        "status": "success", # success or error
+        "status": "success",  # success or error
         "data": {
             "message": "Rehabilitation plan updated successfully"
         }
     }
     return json.dumps(res)
 
+
 @app.route('/api/rehabilitation/plans', methods=["GET"])
 def get_plans() -> Response | str:
-    '''
+    """
     获取康复计划详情接口
     返回参数说明：
     - status (string): 请求状态，值为 "success" 或 "error"
@@ -231,11 +242,11 @@ def get_plans() -> Response | str:
     - description (string): 锻炼项目描述
     - sets (integer): 组数
     - repetitions (integer): 每组次数
-    '''
+    """
     # user_info = request.get_json()
     # print(user_info)
     res = {
-        "status": "success", # success or error
+        "status": "success",  # success or error
         "data": {
             "planId": "plan123",
             "planName": "Example Plan",
@@ -258,30 +269,32 @@ def get_plans() -> Response | str:
     }
     return json.dumps(res)
 
+
 @app.route('/api/rehabilitation/plans', methods=["DELETE"])
 def delete_plans() -> Response | str:
-    '''
+    """
     删除康复计划接口
-    '''
+    """
     # user_info = request.get_json()
     # print(user_info)
     res = {
-        "status": "success", # success or error
+        "status": "success",  # success or error
         "data": {
             "message": "Rehabilitation plan deleted successfully"
         }
     }
     return json.dumps(res)
 
+
 @app.route('/api/rehabilitation/generate-plan', methods=["POST"])
 def generate_plans() -> Response | str:
-    '''
+    """
     自动生成康复训练计划接口
-    '''
+    """
     user_info = request.get_json()
     print(user_info)
     res = {
-        "status": "success", # success or error
+        "status": "success",  # success or error
         "data": {
             "planId": "plan123",
             "planName": "Generated Plan",
@@ -304,18 +317,19 @@ def generate_plans() -> Response | str:
     }
     return json.dumps(res)
 
-#--------------------------------------------------------------
+
+# --------------------------------------------------------------
 #                 2.2 康复知识库模块                            |
-#--------------------------------------------------------------
+# --------------------------------------------------------------
 @app.route('/api/rehabilitation/knowledge/categories', methods=["GET"])
 def get_categories() -> Response | str:
-    '''
+    """
     获取康复知识分类列表接口
-    '''
+    """
     # user_info = request.get_json()
     # print(user_info)
     res = {
-        "status": "success", # success or error
+        "status": "success",  # success or error
         "data": [
             {
                 "categoryId": "category123",
@@ -329,12 +343,13 @@ def get_categories() -> Response | str:
     }
     return json.dumps(res)
 
+
 @app.route('/api/rehabilitation/knowledge', methods=["GET"])
 def get_knowledge() -> Response | str:
-    '''
+    """
     获取康复知识列表接口
-    '''
-    user_info = request.args.get('category','')
+    """
+    user_info = request.args.get('category', '')
     # user_info = request.get_json()
     print(user_info)
     res = {
@@ -344,8 +359,8 @@ def get_knowledge() -> Response | str:
                 "knowledgeId": "knowledge123",
                 "title": "Exercise Techniques for Knee Rehabilitation",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...",
-                "image": "https://example.com/image.jpg",
-                "video": "https://example.com/video.mp4",
+                "image": None,
+                "video": None,
                 "source": "National Institute of Health",
                 "timestamp": "2023-05-31T10:25:42Z"
             },
@@ -362,99 +377,105 @@ def get_knowledge() -> Response | str:
     }
     return json.dumps(res)
 
-#--------------------------------------------------------------
+
+# --------------------------------------------------------------
 #                      2.3 进度跟踪模块                        |
-#--------------------------------------------------------------
+# --------------------------------------------------------------
 @app.route('/api/rehabilitation/progress/<string:userId>', methods=["GET"])
 def get_progress_data(userId) -> Response | str:
-    '''
+    """
     获取康复进度数据接口
-    '''
+    """
     # user_info = request.get_json()
     # print(user_info)
     res = {
-        "status": "success", 
-        "data": { 
-            "userId": "user123", 
-            "totalSessions": 20, 
-            "completedSessions": 15, 
-            "progressPercentage": 75, 
-            "nextSessionDate": "2023-06-10" 
+        "status": "success",
+        "data": {
+            "userId": "user123",
+            "totalSessions": 20,
+            "completedSessions": 15,
+            "progressPercentage": 75,
+            "nextSessionDate": "2023-06-10"
         }
     }
     return json.dumps(res)
+
 
 @app.route('/api/rehabilitation/progress/report/<string:userId>', methods=["GET"])
 def get_progress_report(userId) -> Response | str:
-    '''
+    """
     获取进度报告接口
-    '''
+    """
     # user_info = request.get_json()
     # print(user_info)
     res = {
-        "status": "success", 
-        "data": { 
-            "userId": "user123", 
-            "progressPercentage": "75%", 
-            "report": "您的康复进度良好，已完成75%的训练计划。请继续保持，下一次训练的日期是2023-06-10。" 
+        "status": "success",
+        "data": {
+            "userId": "user123",
+            "progressPercentage": "75%",
+            "report": "您的康复进度良好，已完成75%的训练计划。请继续保持，下一次训练的日期是2023-06-10。"
         }
     }
     return json.dumps(res)
 
+
 @app.route('/api/rehabilitation/progress/update', methods=["PUT"])
 def update_progress() -> Response | str:
-    '''
+    """
     更新康复进度接口
-    '''
+    """
     user_info = request.get_json()
     print(user_info)
     res = {
-        "status": "success", 
+        "status": "success",
         "message": "康复进度更新成功"
     }
     return json.dumps(res)
 
+
 @app.route('/api/rehabilitation/progress/detail/<string:userId>', methods=["GET"])
 def get_progress_detail(userId) -> Response | str:
-    '''
+    """
     获取详细进度数据
-    '''
+    """
     # user_info = request.get_json()
     # print(user_info)
     res = {
-        "status": "success", 
+        "status": "success",
         "data": {
             "userId": "user123",
             "weight": [140.1, 141, 144.3, 142, 145, 147],
-            "O2inblood": [93.4, 94, 94.2, 95, 95, 96]
+            "O2_in_blood": [93.4, 94, 94.2, 95, 95, 96]
         }
     }
     return json.dumps(res)
 
-#--------------------------------------------------------------
+
+# --------------------------------------------------------------
 #                      3.1 药物反馈模块                        |
-#--------------------------------------------------------------
+# --------------------------------------------------------------
 @app.route('/api/medication/feedback', methods=["POST"])
 def submit_medication_feedback() -> Response | str:
-    '''
+    """
     提交药物反馈接口
-    '''
+    """
     user_info = request.get_json()
     print(user_info)
     res = {
-        "status": "success", # success or error
+        "status": "success",  # success or error
         "message": "药物反馈提交成功"
     }
     return json.dumps(res)
 
+
 @app.route('/api/medication/feedbacks/<string:medicationId>', methods=["GET"])
 def get_medication_feedback(medicationId) -> Response | str:
-    '''
+    """
     获取药物反馈列表接口
-    '''
+    """
     # user_info = request.get_json()
     # print(user_info)
-    res =  {
+    res = {
         "status": "success",
         "data": [
             {
@@ -475,30 +496,32 @@ def get_medication_feedback(medicationId) -> Response | str:
     }
     return json.dumps(res)
 
-#--------------------------------------------------------------
+
+# --------------------------------------------------------------
 #                      3.2 问诊反馈模块                        |
-#--------------------------------------------------------------
+# --------------------------------------------------------------
 @app.route('/api/inquiry/feedback', methods=["POST"])
 def submit_inquiry_feedback() -> Response | str:
-    '''
+    """
     提交问诊反馈接口
-    '''
+    """
     user_info = request.get_json()
     print(user_info)
-    res =  {
+    res = {
         "status": "success",
         "message": "问诊反馈提交成功"
     }
     return json.dumps(res)
 
+
 @app.route('/api/inquiry/feedbacks/<string:doctorId>', methods=["GET"])
 def get_inquiry_feedback(doctorId) -> Response | str:
-    '''
+    """
     获取问诊反馈列表接口
-    '''
+    """
     # user_info = request.get_json()
     # print(user_info)
-    res =   {
+    res = {
         "status": "success",
         "data": [
             {
@@ -516,6 +539,7 @@ def get_inquiry_feedback(doctorId) -> Response | str:
         ]
     }
     return json.dumps(res)
+
 
 if __name__ == "__main__":
     app.run(threaded=True, host="127.0.0.1", port=3001, debug=True)
